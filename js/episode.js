2e4e16dd98d9aa13755e3a19790e2d697036f2e1
firebrotherua <firebrotherua@gmail.com>
2024-05-21 09:09:05 +0530

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -7,7 +7,7 @@ const dlapi = "/download/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api100.anime-dex.workers.dev"];
+const AvailableServers = ["https://api1main.firebrotherua.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
